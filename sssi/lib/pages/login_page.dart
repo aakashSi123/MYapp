2d063b123983d9a1d75a4726a905b0852b0937f2
aakashSi123 <susilaorigin@gmail.com>
2023-09-01 13:39:26 +0545

    using splashclr and null entry of usern and pw dne

--- sssi/lib/pages/login_page.dart
+++ sssi/lib/pages/login_page.dart
@@ -12,6 +12,24 @@ class login_page extends StatefulWidget {
 class _login_pageState extends State<login_page> {
   String name = "";
   bool changeButton = false;
+  final _formKey = GlobalKey<FormState>();
+
+  moveToHome(BuildContext context) async {
+    if (_formKey.currentState!.validate()) {
+      setState(() {
+        changeButton = true;
+      });
+
+      await Future.delayed(
+          Duration(seconds: 1)); // click karne ke baad 1 second wait karna hai
+      await Navigator.pushNamed(context, MyRoutes.homeRoute);
+      setState(() {
+        changeButton = false;
+      });
+    }
+    // click karne ke baad kisi new page mei jane ke  liye
+  }
+
   @override
   Widget build(BuildContext context) {
     return Material(
@@ -24,6 +42,7 @@ class _login_pageState extends State<login_page> {
                 fit: BoxFit.cover,
                 height: 350,
               ),
+
               Text(
                 "Welcome $name",
                 style: TextStyle(
@@ -34,86 +53,89 @@ class _login_pageState extends State<login_page> {
               SizedBox(
                 height: 20.0,
               ),
+              // ek box ke jitna ka space chhodne ke liye
+
               Padding(
                 padding: const EdgeInsets.symmetric(
                     vertical: 16.0, horizontal: 32.0),
-                child: Column(
-                  children: [
-                    TextFormField(
-                      decoration: InputDecoration(
-                        hintText: "Enter Username",
-                        labelText: "Username",
-                      ),
-                      onChanged: (value) {
-                        name = value;
-                        setState(() {});
-                      },
-                    ),
-                    TextFormField(
-                      obscureText: true,
-                      decoration: InputDecoration(
-                        hintText: "Enter Password",
-                        labelText: "Password",
-                      ),
-                    ),
-                    SizedBox(
-                      height: 20.0,
-                    ),
-                    // ElevatedButton(
-                    //   child: Text(
-                    //     "login",
-                    //     style: TextStyle(fontSize: 16),
-                    //   ),
-                    //   style: TextButton.styleFrom(
-                    //     minimumSize: Size(100, 50),
-                    //   ),
+                child: Form(
+                  key: _formKey,
+                  child: Column(
+                    children: [
+                      TextFormField(
+                        decoration: InputDecoration(
+                          hintText: "Enter Username",
+                          labelText: "Username",
+                        ),
+                        validator: (value) {
+                          if (value!.isEmpty) {
+                            return "Username cannot be empty";
+                          }
 
-                    //   onPressed: () {
-                    //     Navigator.pushNamed(context, MyRoutes.homeRoute);
-                    //   },
-                    // ),
-                    Material(
-                      child: InkWell( 
-                        //to make the container clickable
-                     // aage ya pichhe button ko color dene ke liye 
-                        onTap: () async { // on tap method ko async kiya hai because kyuki jo await future delay wala function hai usko isi class mei rakhna pdega warna kam nahi karega 
-
-                          setState(() {
-                            changeButton = true;
-                          });
-                        
-                          await Future.delayed(Duration(seconds: 1)); // click karne ke baad 1 second wait karna hai 
-                          Navigator.pushNamed(context, MyRoutes.homeRoute); // click karne ke baad kisi new page mei jane ke  liye 
+                          return null;
                         },
-                        child: AnimatedContainer(
-                          duration: Duration(seconds: 1),
-                          width: changeButton ? 50 : 150,
-                          height: 50,
-                          alignment: Alignment.center,
-                          child: changeButton
-                              ? Icon(
-                                  Icons.done,
-                                  color: Colors.white,
-                                )
-                              : Text(
-                                  "Login",
-                                  style: TextStyle(
+                        onChanged: (value) {
+                          name = value;
+                          setState(() {});
+                        },
+                      ),
+                      TextFormField(
+                        obscureText: true,
+                        decoration: InputDecoration(
+                          hintText: "Enter Password",
+                          labelText: "Password",
+                        ),
+                        validator: (value) {
+                          if (value!.isEmpty) {
+                            return "Password cannot be empty";
+                          } else if (value.length < 8) {
+                            return "Password must be atleast 8";
+                          }
+
+                          return null;
+                        },
+                      ),
+                      SizedBox(
+                        height: 20.0,
+                      ),
+                      Material(
+                        // isko material is liye kara kyuki splash color nahi dikh raha tha
+                        borderRadius:
+                            BorderRadius.circular(changeButton ? 50 : 8),
+                        color: Colors.blue,
+                        child: InkWell(
+                          splashColor: Color.fromARGB(255, 70, 6,
+                              180), //to make the container clickable
+                          // aage ya pichhe button ko color dene ke liye
+                          onTap: () => moveToHome(context),
+                          child: AnimatedContainer(
+                            duration: Duration(seconds: 1),
+                            width: changeButton ? 50 : 150,
+                            height: 50,
+                            alignment: Alignment.center,
+                            child: changeButton
+                                ? Icon(
+                                    Icons.done,
                                     color: Colors.white,
-                                    fontSize: 16,
+                                  )
+                                : Text(
+                                    "Login",
+                                    style: TextStyle(
+                                      color: Colors.white,
+                                      fontSize: 16,
+                                    ),
                                   ),
-                                ),
-                          decoration: BoxDecoration(
-                            color: Colors.lightBlue,
-                            // shape: changeButton
-                            //     ? BoxShape.circle
-                            //     : BoxShape.rectangle,
-                            borderRadius:
-                                BorderRadius.circular(changeButton ? 50 : 8),
+                            // decoration: BoxDecoration(
+                            //   color: Colors.lightBlue,
+                            //   // shape: changeButton
+                            //   //     ? BoxShape.circle
+                            //   //     : BoxShape.rectangle,
+                            // ),
                           ),
                         ),
                       ),
-                    ),
-                  ],
+                    ],
+                  ),
                 ),
               )
             ],
